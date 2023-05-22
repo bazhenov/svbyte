bc854844682f4d287aff13172bcda85211ccb7d0
Denis Bazhenov <dotsid@gmail.com>
2023-05-22 09:13:50 +0700

    Documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,20 @@
 /*!
 This library provides encoding/decoding primitives for Stream VByte encoding.
 
-Stream VByte encoding is a SIMD accelerated algorithm of VarInt decompression. It is used in a search and database
-systems as a way of efficiently store and stream large number of VarInts from a disk or main memory.
+Stream VByte encoding is a SIMD accelerated algorithm of varint decompression. It is used in a search and database
+systems as a way of efficiently store and stream large number of variable length integers from a disk or main memory.
 
-The idea behind VarInt is not to store leading bytes of a number, so large amount of relatively small numbers can be
-stored in a much more compact way. VarInt encoding is frequently used with delta-encoding if numbers are stored in the
+The idea behind virnot is not to store leading bytes of the number, so large amount of relatively small numbers can be
+stored in a much more compact way. Varint encoding is frequently used with delta-encoding if numbers are stored in the
 ascending order. This way all the numbers are smaller by magnitude, hence better compression.
 
-Stream VByte working using two data streams: control stream and data stream. Control stream contains control words (1
-byte each). Each control word describe length of 4 numbers in the data stream (2 bits per number, `00` - length 1,
-`01` - length 2 and so on).
+Stream VByte is a storage format and an algorithm which allows to vectorize compressing and decompressing of numbers
+on a modern CPUs.
 
-- [Decoding billions of integers per second through vectorization][pub] by Daniel Lemire and Leonid Boytsov.
+- [Stream VByte: Faster Byte-Oriented Integer Compression][pub] by Daniel Lemire, Nathan Kurz, and Christoph Rupp
 - [Stream VByte: breaking new speed records for integer compression][blog-post] by Daniel Lemire
 
-[pub]: https://arxiv.org/abs/1209.2137
+[pub]: https://arxiv.org/abs/1709.08990
 [blog-post]: https://lemire.me/blog/2017/09/27/stream-vbyte-breaking-new-speed-records-for-integer-compression/
 */
 use std::{
